5864b9e28c858c3baeb3bba8b45261866f2c4fa8
NorbertGalazka <norbert1996galazka@gmail.com>
2023-01-26 17:23:06 +0100

    remove unnecessary inheritance

--- main.py
+++ main.py
@@ -4,18 +4,17 @@ from settings import Settings
 
 
 class AlienInvasion:
-    def __init__(self,settings):
-        self.settings = settings   # Przypisanie ustawień z klasy Settings do zmiennej
+    def __init__(self,settings,ship):
+        self.ship = ship
+
+        self.settings = settings
 
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height)) # Utworzenie zmiennej, oraz utworzenie okienka gry
 
 
     def run_game(self):
         pygame.init()   # Inicjalizacja gry
-
-        pygame.display.set_caption("Inwazja obcych")   # Nagłówek
-
-        ship = Ship(Settings())
+        pygame.display.set_caption("Inwazja obcych")
 
         while True:
             pygame.time.Clock().tick(60) # Maksymalne FPS
@@ -26,22 +25,23 @@ class AlienInvasion:
 
             self.screen.fill((230, 0, 0))  # Rysowanie tła
 
-            spaceship_position = ship.movement() # Przypisanie pozycji i rozmiaru prostokąta do zmiennej
+            spaceship_position = self.ship.movement() # Przypisanie pozycji i rozmiaru prostokąta
 
-            pygame.draw.rect(self.screen,(0,230,0),spaceship_position)    # Rysowanie prostokąta (wybrany ekran,
-                                                                # kolor prostokąta, wybrany prostokąt i jego położenie)
+            # pygame.draw.rect(self.screen,(0,230,0),spaceship_position)    # Rysowanie prostokąta (wybrany ekran,
+                                                                  # kolor prostokąta, wybrany prostokąt i jego położenie)
+
+            self.screen.blit(self.settings.image,(spaceship_position)) #wstawianie obrazka (obrazek,pozycja)
 
             pygame.display.update()  # Wyswietlenie ostatnio zmodyfikowanego ekranu
 
 
-class Ship(AlienInvasion):
-    def __init__(self,settings):
-        super().__init__(settings)
+class Ship():
+    def __init__(self):
         self.speed = 10
         self.rect_x_position = 265
         self.rect_y_position = 680
-        self.rect_width = 60
-        self.rect_heigth = 60
+        self.rect_width = 75
+        self.rect_height = 68
 
 
     def movement(self):
@@ -58,7 +58,7 @@ class Ship(AlienInvasion):
 
         if keys[pygame.K_UP]:
             self.rect_y_position -= self.speed
-            if self.rect_y_position < - self.rect_heigth:
+            if self.rect_y_position < - self.rect_height:
                 self.rect_y_position = 793
 
         if keys[pygame.K_DOWN]:
@@ -66,12 +66,12 @@ class Ship(AlienInvasion):
             if self.rect_y_position > 793:
                 self.rect_y_position = 0
 
-        return pygame.rect.Rect(self.rect_x_position, self.rect_y_position, self.rect_width, self.rect_heigth)   # tworzenie prostokąta (2 pierwsze zmienne to jego położenie),
+        return pygame.rect.Rect(self.rect_x_position, self.rect_y_position, self.rect_width, self.rect_height)   # tworzenie prostokąta (2 pierwsze zmienne to jego położenie),
                                                                                     # (2 pozostałe to rozmiar)
 
 
+game = AlienInvasion(settings=Settings(), ship=Ship())
 
-game = AlienInvasion(Settings())
 game.run_game()
 
 
