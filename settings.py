ad546acf8dffe1184cdc6b73e6fa2cec4e08659d
NorbertGalazka <norbert1996galazka@gmail.com>
2023-01-31 11:52:10 +0100

    add background

--- settings.py
+++ settings.py
@@ -4,10 +4,18 @@ import pygame
 class Settings:
     def __init__(self):
         """Inicjalizacja ustawień gry"""
-        #Ustawienia ekranu
-        self.rectangle_color = ((0,230,0))
+        self.bg = pygame.image.load("images\space.png")
+        self.image = pygame.image.load("images\spaceship-removebg-preview.png")
+        self.rectangle_color = ((0, 230, 0))
         self.screen_width = 589
         self.screen_height = 793
         self.bg_color = (230,0,0)
         self.window_with_size = pygame.display.set_mode((589, 793))
+        # bullet
+        self.bullet_speed = 10
+        self.bullet_width = 3
+        self.bullet_height = 30
+        self.bullet_color = (255,128,0)
+        self.bullet_start_x = 300
+        self.bullet_start_y = 750
 
