1ee291e4946edd8af785e52592d994c13a023c30
NorbertGalazka <norbert1996galazka@gmail.com>
2023-02-09 16:41:40 +0100

    add a dateclass module

--- settings.py
+++ settings.py
@@ -4,33 +4,43 @@ from dataclasses import dataclass
 
 @dataclass
 class GameSettings:
-
     bg = pygame.image.load("images\space.png")
-    spaceship_img = pygame.image.load("images\spaceship-removebg-preview.png")
-        # self.rectangle_color = (0, 230, 0)
     screen_width = 589
     screen_height = 793
-        # self.bg_color = (230, 0, 0)
-        # self.window_with_size = pygame.display.set_mode((589, 793))
-        # self.spaceship_start_x_position = 265
     screen = pygame.display.set_mode((screen_width, screen_height))
+    left_edge_of_the_screen = 0
 
 
+@dataclass
 class ShipBulletSettings:
-    def __init__(self):
-        self.bullet_speed = 9
-        self.bullet_width = 5
-        self.bullet_height = 15
-        self.bullet_color = (255, 128, 0)
-        self.bullet_start_x = 300
-        self.bullet_start_y = 750
+    bullet_speed = 9
+    bullet_width = 5
+    bullet_height = 15
+    bullet_color = (255, 128, 0)
+    bullet_start_x = 300
+    bullet_start_y = 750
 
 
+@dataclass
 class AlienBulletSettings:
-    def __init__(self):
-        self.bullet_speed = 10
-        self.bullet_width = 7
-        self.bullet_height = 7
-        self.bullet_color = (255, 255, 100)
-        self.bullet_start_x = 300
-        self.bullet_start_y = 750
+    bullet_speed = 10
+    bullet_width = 7
+    bullet_height = 7
+    bullet_color = (255, 255, 100)
+    bullet_start_x = 300
+    bullet_start_y = 750
+
+
+@dataclass
+class AlienSettings:
+    alien_columns = 3
+    alien_rows = 2
+    alien_cooldown = 500
+
+
+@dataclass
+class ShipSettings:
+    spaceship_img = pygame.image.load("images\spaceship-removebg-preview.png")
+    rect_width = spaceship_img.get_width()
+    rect_height = spaceship_img.get_height()
+    speed = 5
