5d490619fde0f38395848b9b43938c9a8e0a97cb
NorbertGalazka <norbert1996galazka@gmail.com>
2023-02-19 14:58:14 +0100

    add a new image

--- settings.py
+++ settings.py
@@ -11,6 +11,23 @@ class GameSettings:
     left_edge_of_the_screen = 0
     game_over_image = pygame.image.load("images/game_over.png")
     start_game_image = pygame.image.load("images/start_game.png")
+    you_win_image = pygame.image.load("images/you_win_r_bg.png")
+
+
+@dataclass
+class AlienSettings:
+    alien_columns = 1
+    alien_rows = 1
+    alien_cooldown = 500
+
+
+@dataclass
+class ShipSettings:
+    spaceship_img = pygame.image.load("images\spaceship-removebg-preview.png")
+    rect_width = spaceship_img.get_width()
+    rect_height = spaceship_img.get_height()
+    speed = 5
+
 
 @dataclass
 class ShipBulletSettings:
@@ -33,16 +50,6 @@ class AlienBulletSettings:
 
 
 @dataclass
-class AlienSettings:
-    alien_columns = 3
-    alien_rows = 2
-    alien_cooldown = 500
-
-
-@dataclass
-class ShipSettings:
-    spaceship_img = pygame.image.load("images\spaceship-removebg-preview.png")
-    rect_width = spaceship_img.get_width()
-    rect_height = spaceship_img.get_height()
-    # print(rect_height)
-    speed = 5
+class BossSettings:
+    boss_speed = 0.5
+    boss_img = pygame.image.load("images\\little_boss.png")
