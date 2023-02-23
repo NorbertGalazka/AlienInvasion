82e675e1fd32f7f7f4146b2155ac3d59a79ac780
NorbertGalazka <norbert1996galazka@gmail.com>
2023-02-23 21:07:49 +0100

    add a new feature

--- Explosion.py
+++ Explosion.py
@@ -1,16 +1,44 @@
+# import pygame
+# from settings import ExplosionSettings, GameSettings
+#
+#
+# class Explosion:
+#     def __init__(self, explosion_x, explosion_y,draw_times):
+#         self.explosion_x = explosion_x
+#         self.explosion_y = explosion_y
+#         self.draw_times = draw_times
+#
+#     def get_explosion_rect(self):
+#         image = pygame.image.load(f"images/burst4.png")
+#         image = pygame.transform.scale(image, (20, 20))
+#
+#         explosion_position = pygame.rect.Rect(self.explosion_x - 31, self.explosion_y - 18,
+#                                               image.get_width(), image.get_height())
+#         # GameSettings.screen.blit(pygame.image.load(f"images/burst{image_num}.png"), explosion_position)
+#         GameSettings.screen.blit(image, explosion_position)
+
 import pygame
 from settings import ExplosionSettings, GameSettings
 
 
 class Explosion:
-    def __init__(self, explosion_x, explosion_y):
-        self.explosion_image = pygame.image.load("images/exp1.png")
-        self.expl_img_width = self.explosion_image.get_width()
-        self.expl_img_height = self.explosion_image.get_height()
+    def __init__(self, explosion_x, explosion_y,draw_times,size):
         self.explosion_x = explosion_x
         self.explosion_y = explosion_y
+        self.draw_times = draw_times
+        self.images = []
+        self.size = size
+        for num in range(1, 6):
+            self.image = pygame.image.load(f"images/burst{num}.png")
+            if self.size == 1:
+                self.image = pygame.transform.scale(self.image,(120,120))
+            self.images.append(self.image)
+        # self.index = 0
+        # self.image = self.images[self.index]
 
-    def get_explosion_rect(self):
-        explosion_position = pygame.rect.Rect(self.explosion_x - 31, self.explosion_y - 18, self.expl_img_width,
-                                              self.expl_img_height)
-        GameSettings.screen.blit(ExplosionSettings.image, explosion_position)
+    def get_explosion_rect(self, index):
+
+        explosion_position = pygame.rect.Rect(self.explosion_x - 31, self.explosion_y - 18,
+                                              self.images[index+1].get_width(), self.images[index+1].get_height())
+
+        GameSettings.screen.blit(self.images[index+1], explosion_position)
