b65fc2f6b5e5f7a487c8564db2241ebbe4e67618
NorbertGalazka <norbert1996galazka@gmail.com>
2023-02-28 11:26:52 +0100

    adding class explosion

--- Explosion.py
+++ Explosion.py
@@ -1,5 +1,5 @@
 import pygame
-from settings import ExplosionSettings, GameSettings
+from settings import GameSettings
 
 
 class Explosion:
@@ -12,14 +12,21 @@ class Explosion:
             self.image = pygame.image.load(f"images/burst{num}.png")
             if self.size == 1:
                 self.image = pygame.transform.scale(self.image,(120,120))
+                self.correct_pos_x = 31
+                self.correct_pos_y = 18
+            if self.size == 2:
+                self.image = pygame.transform.scale(self.image, (35, 35))
+                self.correct_pos_x = 8
+                self.correct_pos_y = 0
+            if self.size == 3:
+                self.image = pygame.transform.scale(self.image, (200, 200))
+                self.correct_pos_x = 60
+                self.correct_pos_y = 40
+
             self.images.append(self.image)
-            # print(self.images)
-        # self.index = 0
-        # self.image = self.images[self.index]
 
     def get_explosion_rect(self, index):
-        # print(self.images[index + 1])
-        explosion_position = pygame.rect.Rect(self.explosion_x - 31, self.explosion_y - 18,
+        explosion_position = pygame.rect.Rect(self.explosion_x - self.correct_pos_x, self.explosion_y - self.correct_pos_y,
                                               self.images[index].get_width(), self.images[index].get_height())
 
         GameSettings.screen.blit(self.images[index], explosion_position)
