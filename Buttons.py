a1febe9b4f49ab5e584d6bd5e55d4563847983f7
NorbertGalazka <norbert1996galazka@gmail.com>
2023-03-06 16:41:18 +0100

    refactoring

--- Buttons.py
+++ Buttons.py
@@ -5,16 +5,20 @@ from settings import GameSettings
 class LostGameButton:
     def __init__(self):
         self.restart_image = pygame.image.load("images/restart_image.png")
+        self.restart_image_hovered = pygame.image.load("images/restart_grey_bcg.png")
         self.restart_button_x_cord = 100
         self.restart_button_y_cord = 360
 
-    def restart_button_is_pressed(self,):
+    def restart_button_is_pressed(self):
         restart_image_hitbox = pygame.Rect(self.restart_button_x_cord, self.restart_button_y_cord,
                                            self.restart_image.get_width(), self.restart_image.get_height())
-        GameSettings.screen.blit(self.restart_image, (self.restart_button_x_cord, self.restart_button_y_cord))
+
         if restart_image_hitbox.collidepoint(pygame.mouse.get_pos()):
+            GameSettings.screen.blit(self.restart_image_hovered, (self.restart_button_x_cord, self.restart_button_y_cord))
             if pygame.mouse.get_pressed()[0]:
                 return True
+        else:
+            GameSettings.screen.blit(self.restart_image, (self.restart_button_x_cord, self.restart_button_y_cord))
 
 
 class MainMenuButton:
